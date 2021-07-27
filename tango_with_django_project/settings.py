976549846e6b52b7206140bf3acbecfd0827ccfc
eddyStace <estace@live.com>
2021-07-27 20:02:53 +0100

    Chapter 4

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -13,7 +13,14 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# Want absolute paths to make code portable
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Reference to new template directory
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+# Pointer to static directory
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# Pointer to media directory
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +62,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -119,3 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+# Media files
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
