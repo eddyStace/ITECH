d1e77941d9eee558e9694d2916c9d7c7cb87ee50
eddyStace <estace@live.com>
2021-07-29 15:05:27 +0100

    Chapter 6 complete

--- rango/admin.py
+++ rango/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-
-
-
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
-admin.site.register(Category)
 admin.site.register(Page, PageAdmin)
+
+# Add in this class to customise the Admin Interface
+# Allows admin interface to populate slug field automatically - less human error!
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+# Update the registration to include this customised interface
+admin.site.register(Category, CategoryAdmin)
--- rango/models.py
+++ rango/models.py
@@ -1,12 +1,20 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    views =  models.IntegerField(default=0)
-    likes =  models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True) # Make sure each slug is unique so that it can be identified
+
+    # Save slugified catagory name to appropriate database table
+    # Slugify replaces white space with - for URL names (best practice)
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = 'categories'
 
     def __str__(self):
         return self.name
